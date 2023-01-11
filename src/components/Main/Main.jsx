3317a77819b2bf208a4e2865c78969c8e8cd2076
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-11 10:09:39 +0200

    fix: css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "../Main/Main.module.css";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
-    <main>
+    <main className={style.main}>
       <Outlet />
     </main>
   );
