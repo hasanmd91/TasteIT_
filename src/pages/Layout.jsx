b18cbcf77fdf28216c581b877dc2121e5b2d6324
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-11 10:23:16 +0200

    fix: css

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Main from "../components/Main/Main";
 import Footer from "../components/Footer/Footer";
-import styles from "../pages/Layout.module.css";
 
 export const Layout = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Main />
       <Footer />
